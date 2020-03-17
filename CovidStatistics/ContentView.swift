24f1ac0b8758906fcd44d5ce55faec93e832ebf3
Francesco-Voto <francesco.voto.8@gmail.com>
2020-03-17 22:55:50 +0100

    First Commit

---
+++ CovidStatistics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidStatistics
+//
+//  Created by Francesco Voto on 17/03/2020.
+//  Copyright © 2020 Francesco Voto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
